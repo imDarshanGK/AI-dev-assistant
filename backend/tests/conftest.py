90fcb37272172717adb72a3d34922da8ea1e7939
Ayushman Raj <ananyashree2633@gmail.com>
2026-06-23 14:38:40 +0530

    style: fix black and isort formatting

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,15 +1,15 @@
 import os
 import sys
 
-import pytest
-from fastapi.testclient import TestClient
-
-from app.main import app
-
 CURRENT_DIR = os.path.dirname(__file__)
 PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, ".."))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
+
+import pytest
+from fastapi.testclient import TestClient
+
+from app.main import app
 
 
 @pytest.fixture
